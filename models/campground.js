2d1ab5407d4416b9a9dcfd01245ec36e5567a758
Chin Chin <sssunri@icloud.com>
2024-07-15 06:15:08 +0800

    add reivews reference for relational data

--- models/campground.js
+++ models/campground.js
@@ -9,6 +9,12 @@ const CampgroundSchema = new Schema({
   price: Number,
   description: String,
   location: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
 });
 
 // export campground model
